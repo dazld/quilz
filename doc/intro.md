2c4fc885a16ea9709bed2e84e853a968db070479
Dan Peddle <dan.peddle@gmail.com>
2014-07-20 10:06:16 +0200

    base

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to quilz
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/quilz/core_test.clj
@@ -0,0 +1,7 @@
+(ns quilz.core-test
+  (:require [clojure.test :refer :all]
+            [quilz.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
